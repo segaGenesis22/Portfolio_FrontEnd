0f7ef1d4dba33434873182068dd8f044acb3f2fa
iGen22 <guerratavarezg@gmail.com>
2023-02-19 01:51:18 -0500

    updated files

--- recruiters.js
+++ recruiters.js
@@ -1,26 +1,35 @@
 const postBtn = document.getElementById('post')
-const input = document.getElementById('inputName')
+const getBtn = document.getElementById('get')
 
 const dbUrl = 'http://localhost:3000/recruiters'
 
 postBtn.addEventListener('click', postInfo)
+getBtn.addEventListener('click', getInfo)
 
+async function getInfo(e) {
+    e.preventDefault()
+    const res = await fetch(dbUrl, {
+        method: 'GET', 
+    })
+    const data = await res.json()
+    console.log(data)
+}
 
 async function postInfo(e) {
     e.preventDefault()
     const data = {
         name: "Genesis",
-        email: "guerra@google.com",
-        number: "9783257333",
-        employer: "google"
+        email: "gurra@google.com",
+        number: "9784296264",
+        employer: "Google"
     }
     const res = await fetch(dbUrl, {
         method: 'POST', 
         mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json',
+            'Accept': 'application/json'
         },
         body: JSON.stringify(data)
     })
-    console.log(res)
 }
