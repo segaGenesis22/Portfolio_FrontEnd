f0fe59c4a3b0d8de52492da37dac38d13ecb2254
iGen22 <guerratavarezg@gmail.com>
2023-03-25 13:48:52 -0400

    updated styles of frontend

--- recruiters.js
+++ recruiters.js
@@ -1,5 +1,4 @@
 const postBtn = document.getElementById('post')
-const getBtn = document.getElementById('get')
 var passedName = document.getElementById('name')
 var passedEmail = document.getElementById('email')
 var passedNumber = document.getElementById('number')
@@ -8,20 +7,10 @@ var passedEmployer = document.getElementById('employer')
 const dbUrl = new URL('http://localhost:3000/recruiters')
 
 postBtn.addEventListener('click', postInfo)
-getBtn.addEventListener('click', getInfo)
 passedName.addEventListener('change', () => passedName = passedName.value)
 passedEmail.addEventListener('change', () => passedEmail = passedEmail.value)
 passedNumber.addEventListener('change', () => passedNumber = passedNumber.value)
 passedEmployer.addEventListener('change', () => passedEmployer = passedEmployer.value)
-
-async function getInfo(e) {
-    e.preventDefault()
-    const res = await fetch(dbUrl, {
-        method: 'GET', 
-    })
-    const data = await res.json() 
-    console.log(data)
-}
 
 async function postInfo(e) {
     e.preventDefault()
